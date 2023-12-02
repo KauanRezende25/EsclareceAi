56fe769cec07a4b2c8af73ea1eee2c1584072020
Kauan <kauanrezende146@gmail.com>
2023-12-02 15:26:55 -0300

    banco luiz

--- Model/Banco.php
+++ Model/Banco.php
@@ -1,8 +1,8 @@
 <?php
-        $hostname = "localhost";
-        $bancodedados = "esclarece";
-        $usuario = "root";
-        $senha  = "08022001Jeongin*";
+        $hostname = "162.240.17.101";
+        $bancodedados = "projetos_INF2023_G5";
+        $usuario = "projetos_krezende";
+        $senha  = "&fr9#[Qpi8DQ";
         
         $pdo= new PDO ("mysql:host=$hostname;dbname=$bancodedados","$usuario","$senha");
     
